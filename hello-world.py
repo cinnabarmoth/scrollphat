e42bcbc873d7976a6ef03c99cd734d8d9a5ac4c9
cinnabarmoth <cinnabarmoth@hotmail.co.uk>
2016-03-11 23:53:32 +0000

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import scrollphat
+import time
+import sys
+
+scrollphat.write_string("Hello, World");
+
+while True:
+  try:
+      scrollphat.scroll()
+      time.sleep(0.3)
+    except KeyboardInterrupt:
+    scrollphat.clear()
+    sys.exit(-1)
+      
